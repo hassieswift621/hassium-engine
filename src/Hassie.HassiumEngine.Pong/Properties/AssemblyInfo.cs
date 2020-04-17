309711ccf4529be994e706ffb87f575f1073622b
hassieswift621 <hey@hassie.ninja>
2020-04-17 21:55:18 +0100

    Switch to MG Nuget pkg

--- src/Hassie.HassiumEngine.Pong/Properties/AssemblyInfo.cs
+++ src/Hassie.HassiumEngine.Pong/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Hassie.HassiumEngine.Pong")]
-[assembly: AssemblyProduct("Hassie.HassiumEngine.Pong")]
+[assembly: AssemblyTitle("Pong")]
+[assembly: AssemblyDescription("Pong")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyProduct("Hassium Engine")]
+[assembly: AssemblyCopyright("Copyright ©2020 Hassie.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b7c81ed2-6b23-4cb9-8517-cda2fff58c7f")]
+[assembly: Guid("89b687f4-850e-4cf1-8550-7d3c8c468746")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- src/Hassie.HassiumEngine/Properties/AssemblyInfo.cs
+++ src/Hassie.HassiumEngine/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Hassium Engine")]
-[assembly: AssemblyProduct("Hassium Engine")]
+[assembly: AssemblyDescription("Hassium Engine")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("©2019 Hassie.")]
+[assembly: AssemblyProduct("Hassium Engine")]
+[assembly: AssemblyCopyright("Copyright ©2019-2020 Hassie.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2855f2bb-e72b-4af6-a1ed-47115519c675")]
+[assembly: Guid("0eee049b-32a6-4391-968f-f20c7d66aa73")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0.0")]
